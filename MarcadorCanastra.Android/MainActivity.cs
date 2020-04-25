d0e75875231e457b19b0163a7c9008da0c601b82
Jorge Censi <jorgecensi@gmail.com>
2020-04-26 00:40:17 +1000

    update names and icons

--- MarcadorCanastra.Android/MainActivity.cs
+++ MarcadorCanastra.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace MarcadorCanastra.Droid
 {
-    [Activity(Label = "MarcadorCanastra", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Marcador Canastra", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
--- MarcadorCanastra.Android/Properties/AssemblyInfo.cs
+++ MarcadorCanastra.Android/Properties/AssemblyInfo.cs
@@ -13,11 +13,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("MarcadorCanastra.Android")]
+[assembly: AssemblyTitle("Marcador Canastra")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("MarcadorCanastra.Android")]
+[assembly: AssemblyProduct("Marcador Canastra")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
