d96aa393ca77c4214a0dfdd9cd4e707bab30c1ec
Jorge Censi <jorgecensi@gmail.com>
2020-05-30 17:58:22 +1000

    add app themes

---
+++ MarcadorCanastra/ViewModels/SettingsViewModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using MarcadorCanastra.Themes;
+using MvvmHelpers;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace MarcadorCanastra.ViewModels
+{
+    public class SettingsViewModel : BaseViewModel
+    {
+
+        public bool IsDarkmode {
+            get => Preferences.Get("IsDarkMode", false);
+
+            set {
+                Preferences.Set("IsDarkMode", value);
+                ICollection<ResourceDictionary> mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+                if (mergedDictionaries != null)
+                {
+                    mergedDictionaries.Clear();
+
+                    switch (value)
+                    {
+                        case true:
+                            mergedDictionaries.Add(new DarkTheme());
+                            break;
+                        case false:
+                        default:
+                            mergedDictionaries.Add(new LightTheme());
+                            break;
+                    }
+                }
+
+                OnPropertyChanged(nameof(IsDarkmode));
+
+            }
+        }
+
+        public SettingsViewModel()
+        {           
+            
+        }
+    }
+}
---
+++ MarcadorCanastra/Views/SettingsPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MarcadorCanastra.ViewModels;
+using Xamarin.Forms;
+
+namespace MarcadorCanastra.Views
+{
+    public partial class SettingsPage : ContentPage
+    {
+        public SettingsViewModel ViewModel { get; set; }
+        public SettingsPage()
+        {
+            InitializeComponent();
+            BindingContext = ViewModel = new SettingsViewModel();
+        }
+    }
+}
