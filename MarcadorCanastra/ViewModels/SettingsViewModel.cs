161b3a275201a0cf0b5bbbd25be8c8ed80f3aff9
Jorge Censi <jorgecensi@gmail.com>
2020-05-31 22:44:39 +1000

    add menu icons and settings

--- MarcadorCanastra/ViewModels/SettingsViewModel.cs
+++ MarcadorCanastra/ViewModels/SettingsViewModel.cs
@@ -9,7 +9,6 @@ namespace MarcadorCanastra.ViewModels
 {
     public class SettingsViewModel : BaseViewModel
     {
-
         public bool IsDarkmode {
             get => Preferences.Get("IsDarkMode", false);
 
@@ -37,9 +36,54 @@ namespace MarcadorCanastra.ViewModels
             }
         }
 
-        public SettingsViewModel()
-        {           
-            
+        public bool NoLimit {
+            get => Preferences.Get("MaxPoints", 0)==0;
+            set {
+                if (value)
+                {
+                    Preferences.Set("MaxPoints", 0);
+                }
+                
+            }
+        }
+        public bool Max3000 {
+            get => Preferences.Get("MaxPoints", 0) == 3000;
+            set
+            {
+                if (value)
+                {
+                    Preferences.Set("MaxPoints", 3000);
+                }
+            }
+                
+        }
+        public bool Max4000 {
+            get => Preferences.Get("MaxPoints", 0) == 4000;
+            set {
+                if (value)
+                {
+                    Preferences.Set("MaxPoints", 4000);
+                }
+            } 
+        }
+        public bool Max5000 {
+            get => Preferences.Get("MaxPoints", 0) == 5000;
+            set
+            {
+                if (value)
+                {
+                    Preferences.Set("MaxPoints", 5000);
+                }
+            }
+            }
+        public bool Max8000 {
+            get => Preferences.Get("MaxPoints", 0) == 8000;
+            set {
+                if (value)
+                {
+                    Preferences.Set("MaxPoints", 8000);
+                }
+            } 
         }
     }
 }
