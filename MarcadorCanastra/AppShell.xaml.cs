1cb046858b03cb333d3d1ec20005018c6844f31e
Jorge Censi <jorgecensi@gmail.com>
2020-06-08 17:45:32 +1000

    add share button

--- MarcadorCanastra/AppShell.xaml.cs
+++ MarcadorCanastra/AppShell.xaml.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace MarcadorCanastra
 {
     public partial class AppShell : Xamarin.Forms.Shell
     {
+
+        public ICommand ShareUriCommand => new Command(async () => await ShareUri());
+
         public AppShell()
         {
             InitializeComponent();
+            BindingContext = this;
+        }
+
+
+        public async Task ShareUri()
+        {
+            await Share.RequestAsync(new ShareTextRequest
+            {
+                Uri = "https://play.google.com/store/apps/details?id=com.jorgecensi.marcadorcanastra",
+                Title = "Compartilhe este app",
+                Subject = "Marcador de Canastra",
+                Text = "Olha que legal este Marcador de Canastra"
+
+            });
         }
     }
 }
+
